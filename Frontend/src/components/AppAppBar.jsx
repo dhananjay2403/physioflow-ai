9002ecd41835767db5f7731d91c4b205ed6aee88
lishaangral <lishaangral@gmail.com>
2025-04-23 23:29:04 +0530

    Removing unused files

--- Frontend/src/components/AppAppBar.jsx
+++ Frontend/src/components/AppAppBar.jsx
@@ -11,7 +11,7 @@ import MenuItem from '@mui/material/MenuItem';
 import Drawer from '@mui/material/Drawer';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
-import Sitemark from './SitemarkIcon';
+import Sitemark from './Logo';
 
 const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   display: 'flex',
